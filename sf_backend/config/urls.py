bcabb917f0c99658b94c63b0ddfd8fff0161ca55
Nguyễn Hoài Linh <7oaii4inhh@gmail.com>
2025-10-01 22:51:10 +0700

    feat(sf_backend): config be and database

--- sf_backend/config/urls.py
+++ sf_backend/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sf_backend project.
+URL configuration for SmartFarming project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
